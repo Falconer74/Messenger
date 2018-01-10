c9ca99b533db8f8b37b84c8213dee01d3bac7b88
Artem Strelcov Oleksandrovich <artemstrelcov2001@gmail.com>
2018-01-10 14:10:10 +0200

    Primary entities

---
+++ Messenger/ServiceLib/Entities/Membership.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLib.Entities
+{
+    public class Membership
+    {
+        public int MembershipId { get; set; }
+        public int UserProfileId { get; set; }
+        public int DialogId { get; set; }
+
+        public UserProfile Profile { get; set; }
+        public Dialog Dialog { get; set; }
+        public ICollection<Message> Messages { get; set; }
+
+        public Membership()
+        {
+            Messages = new List<Message>();
+        }
+    }
+}
