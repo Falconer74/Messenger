670b28ba4660836576d3a490be72856167ca5be4
Artem Strelcov Oleksandrovich <artemstrelcov2001@gmail.com>
2018-01-10 12:25:40 +0200

    commit

---
+++ Messenger/ServiceLib/Entities/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLib.Entities
+{
+    public class UserProfile
+    {
+        [Key]
+        [ForeignKey("Account")]
+        public int AccountId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public Account Account { get; set; }
+    }
+}
